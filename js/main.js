c576070816fc661696818b5e7f02a651c919464b
Quinn <klassenq@gmail.com>
2018-03-01 07:23:13 -0800

    Removed some globals and improved naming of a few local variables.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage(); // Make sure the browser supports WebGL
 
 var container;
-var tub_mesh;
 var stats;
 var scene;
 var camera;
 var renderer;
-var material;
-var mesh;
-var geometry;
+var water_mesh;
 
 var worldWidth = 124;
 var surface_width = 1000;
 var clock = new THREE.Clock();
 
 var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
+
 
 velocity_field = createArray(worldWidth, worldWidth);
 height_field = createArray(worldWidth, worldWidth);
@@ -61,44 +58,46 @@ function simulation_step(heights, velocities , dt)
 
 // Sets up the Simulation
 function init(){
-    //
+    // 
     init_conditions(height_field, velocity_field);
     //
     container = document.createElement( 'div' );
     document.body.appendChild( container );
-    stats = new Stats();
+    stats = new Stats(); // Gives the framerate in the top corner 
     container.appendChild( stats.dom );
     //
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
+    //
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );
     camera.position.y = 150;
     camera.position.x = -550;
-    
+    camera.position.z = 5;
+    // Set up the first person controls should proably be changed to use keyboard
     controls = new THREE.FirstPersonControls( camera );
     controls.movementSpeed =250;
     controls.lookSpeed = 0.1;
-    
+    //
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     
-    geometry = new THREE.PlaneGeometry( surface_width, surface_width, worldWidth, worldWidth );
-    geometry.rotateX( - Math.PI / 2 );
+    var water_geometry = new THREE.PlaneGeometry( surface_width, surface_width, worldWidth, worldWidth );
+    water_geometry.rotateX( - Math.PI / 2 );
+    set_heights(height_field, water_geometry.vertices);
+    
+    var water_texture = new THREE.TextureLoader().load( "textures/water.jpg" );
+    water_texture.wrapS = water_texture.wrapT = THREE.RepeatWrapping;
+    water_texture.repeat.set( 5, 5 );
 
-    set_heights(height_field, geometry.vertices);
-
-    var texture = new THREE.TextureLoader().load( "textures/water.jpg" );
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 5, 5 );
-
-    material = new THREE.MeshBasicMaterial( { color: 0x0000ff, map: texture } );
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    var water_material = new THREE.MeshBasicMaterial( { color: 0x0000ff, map: water_texture } );
+    
+    water_mesh = new THREE.Mesh( water_geometry, water_material );
+    scene.add( water_mesh );
     //
     //create_container(surface_width, 100, scene)
 
-    camera.position.z = 5;
+
     window.addEventListener( 'resize', onWindowResize, false );
     window.addEventListener( 'click', onDocumentMouseDown, false );
 
@@ -114,14 +113,14 @@ function onWindowResize() {
 }
 // Handles Mouse Click
 function onDocumentMouseDown( event ) { 
-    
+    var mouse = new THREE.Vector2();
     // calculate mouse position in normalized device coordinates
 	// (-1 to +1) for both components
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     raycaster.setFromCamera( mouse, camera );
     //
-    var intersect_data = raycaster.intersectObject(mesh, false);
+    var intersect_data = raycaster.intersectObject(water_mesh, false);
     if(intersect_data.length > 0){
         var face_hit = intersect_data[0].face;
         var vert_index = [face_hit.a, face_hit.b, face_hit.c];
@@ -142,8 +141,8 @@ function render(){
     if(document.visibilityState == "visible"){
         simulation_step(height_field, velocity_field, delta); // Performs a update of the simulation
     }
-    set_heights(height_field, geometry.vertices);
-    mesh.geometry.verticesNeedUpdate = true;
+    set_heights(height_field, water_mesh.geometry.vertices);
+    water_mesh.geometry.verticesNeedUpdate = true;
     controls.update( delta );
     renderer.render(scene, camera);
 }
